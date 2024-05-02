5a96ef29f4e01d3a25c8094f0d190e84117cd495
imurata <61151479+imurata@users.noreply.github.com>
2024-05-02 13:36:53 +0900

    Update 99_random_access.sh

--- 99_random_access.sh
+++ 99_random_access.sh
@@ -2,8 +2,16 @@
 set -x
 PROXY_HOST=localhost
 PROXY_PORT=28000
+ROUTE_ENDPOINT="${PROXY_HOST}:${PROXY_PORT}"
 
 while : ; do
-	curl $PROXY_HOST:$PROXY_PORT
+	RUN_TIMES=$(($RANDOM%10))
+	for ((i=0; i < $RUN_TIMES; i++)); do
+		if [ $i -eq 1 ]; then
+			curl $ROUTE_ENDPOINT/error-route > /dev/null 2>&1
+		fi
+		curl $ROUTE_ENDPOINT > /dev/null 2>&1
+		sleep .5
+	done
 	sleep $(($RANDOM%60))
 done
